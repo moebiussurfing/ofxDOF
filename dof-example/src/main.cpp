af06b26f88ed3871570ad575c303169aa6006503
danb <dan@buzzo.com>
2018-11-18 12:48:06 +0100

    update to of_0.10.x

--- dof-example/src/main.cpp
+++ dof-example/src/main.cpp
@@ -2,11 +2,11 @@
 
 //--------------------------------------------------------------
 int main(){
-    ofGLWindowSettings settings;
-    settings.setGLVersion(3,2);
-//    settings.setGLVersion(2, 1);
-    settings.width = 1280;
-    settings.height = 720;
-    ofCreateWindow(settings);
-	ofRunApp(new testApp()); // start the app
+    ofSetupOpenGL(1024,768,OF_WINDOW);            // <-------- setup the GL context
+    
+    // this kicks off the running of my app
+    // can be OF_WINDOW or OF_FULLSCREEN
+    // pass in width and height too:
+    ofRunApp(new testApp());
+
 }
